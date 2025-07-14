e522dde8536ad1c3386e4d004ab27059495e01d6
shin-no1 <shinhe524@gmail.com>
2025-07-14 16:20:29 +0900

    feat: vite.config.ts 외부접속 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['newsgpt.know-that.dev', 'localhost'],
+    host: '0.0.0.0', // 외부 접속 허용
+    port: 5173,
+  },
   plugins: [
       react(),
   ],
